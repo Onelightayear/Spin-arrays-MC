cc6535390fe1d72b2ff928331129d2bcd8e2635f
Onelightyear <slava.kutuzzi@gmail.com>
2024-06-23 20:05:21 +0300

    First commit

---
+++ 1D spiin array classical.py
@@ -0,0 +1,79 @@
+import random as rnd
+from math import exp as exp
+from matplotlib import pyplot as plt
+
+def Energy(arr, J):
+    E = 0
+    for i in range(len(arr)):
+        if i == len(arr)-1:
+            E += J*0.25*arr[i]*arr[0]
+        else:
+            E += J*0.25*arr[i]*arr[i+1]
+    return E
+
+def S_tot(arr):
+    S = 0
+    for i in arr:
+        S+=0.5*i
+    return S
+
+class Spin_chain():
+    def __init__(self, Num_spins=10):
+        self.Num_spins = Num_spins
+        self.start_state = [rnd.randrange(-1,2,2) for i in range(Num_spins)]
+        self.J = float(input('J value:'))
+        self.start_E = Energy(self.start_state, self.J)
+        self.E_arr = []
+        self.S_t = []
+
+    def update(self):
+        old_state = self.start_state
+        old_E = self.start_E
+        counter = 0
+        old_S = S_tot(old_state)
+
+        while(counter < 10000):
+            spin_change = rnd.randint(0,self.Num_spins-1)
+            new_state = old_state
+            new_state[spin_change] = -1*new_state[spin_change]
+            new_E = Energy(new_state, self.J)
+            new_S = S_tot(new_state)
+
+            #making a state transfer choice
+            a = rnd.uniform(0,1)
+            if new_E < old_E:
+                k = 0
+            else:
+                k=1
+            if k < a:
+                old_E = new_E
+                old_S = new_S
+                old_state = new_state
+                self.S_t.append(old_S)
+                # print(old_state)
+                self.E_arr.append(old_E)
+                
+            else:
+                # print(old_state)
+                self.E_arr.append(old_E)
+                self.S_t.append(old_S)
+            counter +=1
+
+J_pos = Spin_chain()
+J_neg = Spin_chain()
+
+J_pos.update()
+J_neg.update()
+
+fig, axs = plt.subplots(2,2)
+axs[0,0].plot(J_pos.E_arr, label='Energy, J = +5')
+axs[0,0].legend()
+axs[0,1].plot(J_pos.S_t, label='Total spin, J = '+str(J_pos.J), color='orange')
+axs[0,1].legend()
+axs[1,0].plot(J_neg.E_arr, label='Energy, J = -5')
+axs[1,0].legend()
+axs[1,1].plot(J_neg.S_t, label='Total spin, J = '+str(J_neg.J), color='orange')
+axs[1,1].legend()
+
+plt.show()
+
